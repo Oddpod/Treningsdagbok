d9db66c9993e8410ad4cab37778afb0289e3f1d8
Oddpod <oddh123@gmail.com>
2016-03-13 14:03:26 +0100

    meh

--- Treningsdagbok/src/jbdcdemo/Driver.java
+++ Treningsdagbok/src/jbdcdemo/Driver.java
@@ -1,4 +1,6 @@
 package jbdcdemo;
+import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,7 +9,13 @@ import java.sql.Statement;
 public class Driver {
 
 	public static void main(String[] args) {
-		
+
+        java.util.Date dt = new java.util.Date();
+
+        java.text.SimpleDateFormat sdf =
+                new java.text.SimpleDateFormat("1999-01-12 23:01:24");
+
+        String CT = sdf.format(dt);
 		String url = "jdbc:mysql://localhost:3306/treningsdagbokdb";
 		String user = "root";
 		String password = "AVGvisualstudio123?";
@@ -19,13 +27,20 @@ public class Driver {
 			Statement myStmt = myConn.createStatement();
 			//3. Execute SQL query
 			ResultSet myRs = myStmt.executeQuery("select * from treningsøkt");
+            int i = 3;
 			String sql = "insert into treningsøkt "
-					+ "(idtreningsøkt, dato/tidspunkt, varighet, personlig_form, notat, prestasjon )"
-					+ " values(2, 31.12.1999, 45, 5, 'OH MY GOD', 9)";
-			myStmt.executeUpdate(sql);
-			//4. Process the result set
-			while (myRs.next()){
-				System.out.println(myRs.getString("dato/tidspunkt") + ", " + myRs.getString("prestasjon") + ", " + myRs.getString("notat"));
+					+ "(idtreningsøkt, datotid, varighet, personlig_form, notat, prestasjon )"
+					+ " values('', '1999.01.12 23:54:12', '45', '5', 'OH MY GOD', '9')";
+            try {
+                myStmt.executeUpdate(sql);
+            } catch (Exception e) {
+                throw new MySQLIntegrityConstraintViolationException("meh");
+            }
+            i++;
+			// 4. Process the result set
+            ResultSet myRsi = myStmt.executeQuery("select * from treningsøkt");
+			while (myRsi.next()){
+				System.out.println(myRsi.getString("datotid") + ", " + myRsi.getString("prestasjon") + ", " + myRsi.getString("notat"));
 			}
 		}
 		catch (Exception exc){
