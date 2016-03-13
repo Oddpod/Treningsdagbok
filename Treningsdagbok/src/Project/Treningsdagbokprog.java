6fd1a5e3aef290c8d5a4636cd308990519034ca4
Oddpod <oddh123@gmail.com>
2016-03-13 14:31:39 +0100

    meh

---
+++ Treningsdagbok/src/Project/Treningsdagbokprog.java
@@ -0,0 +1,28 @@
+package Project;
+
+import java.util.Scanner;;
+/**
+ * Created by Acer on 13.03.2016.
+ */
+public class Treningsdagbokprog {
+
+    public static void main(String[] args) {
+        java.util.Date dt = new java.util.Date();
+
+        java.text.SimpleDateFormat sdf =
+                new java.text.SimpleDateFormat("1999-01-12 23:01:24");
+
+        String CT = sdf.format(dt);
+        String url = "jdbc:mysql://localhost:3306/treningsdagbokdb";
+        String user = "student";
+        String password = "student";
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+
+
+
+
+}
