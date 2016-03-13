9917387243496c746759efdb849110a77ee7e82b
Oddpod <oddh123@gmail.com>
2016-03-13 13:11:01 +0100

    meh

--- Treningsdagbok/src/jbdcdemo/Driverdemo.java
+++ Treningsdagbok/src/jbdcdemo/Driverdemo.java
@@ -2,7 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-public class Driver {
+
+public class Driverdemo {
 	public static void main(String[] args) throws SQLException {
 		String url = "jdbc:mysql://localhost:3306/demo";
 		String user = "student";
