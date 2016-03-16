86a94676d0f746f9b2e826800c0cbc617c5c5159
Oddpod <oddh123@gmail.com>
2016-03-16 18:43:30 +0100

    Added files via upload

---
+++ treningsdagbokdb/treningsdagbokdb_resultat.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: treningsdagbokdb
+-- ------------------------------------------------------
+-- Server version	5.7.11-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `resultat`
+--
+
+LOCK TABLES `resultat` WRITE;
+/*!40000 ALTER TABLE `resultat` DISABLE KEYS */;
+INSERT INTO `resultat` VALUES ('Løp','30',1,2),('Løp','30',2,2),('Løp','30',3,2),('Løp','30',4,2),('Løp','30',5,2),('Løp','30',6,2),('Løp','20',7,3),('Løp','30',8,2),('Løp','20',9,3),('Løp','30',10,2),('Løp','30',11,2),('Løp','20',12,2),('Løp','20',13,2),('Løp','20',14,2),('Markløft','15',15,2),('Løp','20',16,2),('Løp','20',17,1),('Løp','20',18,1),('Løp','20',19,2),('Løp','20',20,2),('Bla','20',21,2),('Styrke','20',22,2),('Styrke','20',23,2),('Løp','20',24,1),('Løp','20',25,2),('Løp','50',26,2),('Løp','50',27,2),('Løp','50',28,2),('Løp','50',29,2),('Løp','50',30,2),('Løp','50',31,2),('Løp','50',32,2),('Løp','50',33,2),('Løp','50',34,2),('Løp','50',35,2),('Løp','50',36,2),('Løp','50',37,2);
+/*!40000 ALTER TABLE `resultat` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-16 18:38:58
